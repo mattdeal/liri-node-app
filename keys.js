015278564e51becda232688697f60b01151e4042
mwdeal1 <mwdeal1@gmail.com>
2017-02-06 17:33:26 -0500

    Initial Commit

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'Mg1PAj0DB6ngFzRsmWkIZzoCR',
+  consumer_secret: 'IJ8Ovnewron12uaAlkeVJz7IhviT0nPBdMfarP5M9LNWSMYeOi',
+  access_token_key: '105255171-aSIFnX5F5Ozqa7qLum3cmNU0olsuhDk4vnjMbant',
+  access_token_secret: 'SrowuqS0uslpIlES7QBt37AoHH4aEBBmaIfSS4eDpiYb1',
+}
---
+++ liri.js
@@ -0,0 +1,4 @@
+//todo: load keys.js
+
+//todo: get command
+//todo: inside get command, if args are passed, handle args
